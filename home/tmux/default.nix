84891fb5f126156eacedcaf1306a639f6f5c1e6f
ferrazzo <luca733@gmail.com>
2025-04-03 21:47:44 +0200

    fix: fix term colors in tmux

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -2,7 +2,6 @@
 {
   programs.tmux = {
     enable = true;
-    terminal = "xterm-256color";
     extraConfig = builtins.readFile ./config/tmux-${userTheme}.conf;
     # plugins = with pkgs; [
     #   tmuxPlugins.nord
