d233b4fdaaf915b8c55d5516e936c42e3a782cca
ferrazzo <luca733@gmail.com>
2025-04-09 07:28:37 +0200

    feat: add dart functionality and improve tmux config

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -3,7 +3,8 @@
   programs.tmux = {
     enable = true;
     terminal = "xterm-256color";
-    extraConfig = builtins.readFile ./config/tmux-${userTheme}.conf;
+    extraConfig =
+      builtins.readFile ./config/tmux-${userTheme}.conf + "\n" + builtins.readFile ./config/tmux.conf;
     # plugins = with pkgs; [
     #   tmuxPlugins.nord
     # ];
