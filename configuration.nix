f236fcca8d23d7f1b814d0a2fcf1516d88958ec6
ferrazzo <luca733@gmail.com>
2025-04-28 21:21:31 +0200

    feat: add syncthing

--- configuration.nix
+++ configuration.nix
@@ -135,7 +135,6 @@
     xclip
     gnugrep
     ripgrep
-    syncthing
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
