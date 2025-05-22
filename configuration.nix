637e744c9272d6cff31726a38796ef8263ba79be
ferrazzo <luca733@gmail.com>
2025-05-22 21:29:32 +0200

    feat: set intl variant

--- configuration.nix
+++ configuration.nix
@@ -78,18 +78,18 @@
       };
     };
   };
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbVariant = "intl";
+  };
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
