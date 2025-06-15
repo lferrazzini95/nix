84bf53da7026dc8f0c710c70c4e89a22081ac80b
ferrazzo <luca733@gmail.com>
2025-06-15 22:20:36 +0200

    fix: add blink signature

--- home/apps/nvim/config/init.lua
+++ home/apps/nvim/config/init.lua
@@ -10,17 +10,17 @@ vim.g.mapleader = " " -- Sets <Space> as the leader key
 vim.o.updatetime = 300
 
 vim.diagnostic.config({
-	virtual_text = false,
-	float = {
-		focusable = false,
-		border = "rounded",
-		source = "if_many",
-		header = "",
-		prefix = "",
-	},
-	signs = true,
-	underline = false,
-	update_in_insert = false, -- Don't update diagnostics while typing
+  virtual_text = false,
+  float = {
+    focusable = false,
+    border = "rounded",
+    source = "if_many",
+    header = "",
+    prefix = "",
+  },
+  signs = true,
+  underline = false,
+  update_in_insert = false, -- Don't update diagnostics while typing
 })
 
 ----------------------
@@ -28,7 +28,7 @@ vim.diagnostic.config({
 ----------------------
 -- Set clipboard
 vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
+  vim.opt.clipboard = "unnamedplus"
 end)
 
 -- Set tab settings
@@ -54,10 +54,10 @@ vim.api.nvim_set_keymap("n", "<C-k>", ":resize -2<CR>", { noremap = true, silent
 vim.api.nvim_set_keymap("n", "<Leader>us", ":%!sort<CR>", { noremap = true, silent = true, desc = "[S]ort Lines" })
 
 vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>ut",
-	":belowright split | resize " .. math.floor(vim.o.lines / 3) .. " | terminal<CR>",
-	{ noremap = true, silent = true, desc = "[T]erminal" }
+  "n",
+  "<Leader>ut",
+  ":belowright split | resize " .. math.floor(vim.o.lines / 3) .. " | terminal<CR>",
+  { noremap = true, silent = true, desc = "[T]erminal" }
 )
 
 ---------------------------------
@@ -65,13 +65,13 @@ vim.api.nvim_set_keymap(
 ---------------------------------
 require("render-markdown").setup({})
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>tm",
-	":RenderMarkdown toggle<CR>",
-	{ noremap = true, silent = true, desc = "[M]arkdown" }
+  "n",
+  "<leader>tm",
+  ":RenderMarkdown toggle<CR>",
+  { noremap = true, silent = true, desc = "[M]arkdown" }
 )
 vim.keymap.set("n", "<leader>tl", function()
-	vim.wo.number = not vim.wo.number
+  vim.wo.number = not vim.wo.number
 end, { noremap = true, silent = true, desc = "[L]inenumbers" })
 
 -------------------------------------
@@ -88,21 +88,21 @@ vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[W]ord" })
 -- Configure bufferlines --
 ---------------------------
 require("bufferline").setup({
-	options = {
-		separator_style = "thin",
-	},
+  options = {
+    separator_style = "thin",
+  },
 })
 vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>bn",
-	":BufferLineCycleNext<CR>",
-	{ noremap = true, silent = true, desc = "[N]ext" }
+  "n",
+  "<Leader>bn",
+  ":BufferLineCycleNext<CR>",
+  { noremap = true, silent = true, desc = "[N]ext" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>bp",
-	":BufferLineCyclePrev<CR>",
-	{ noremap = true, silent = true, desc = "[P]revious" }
+  "n",
+  "<Leader>bp",
+  ":BufferLineCyclePrev<CR>",
+  { noremap = true, silent = true, desc = "[P]revious" }
 )
 vim.api.nvim_set_keymap("n", "<Leader>bs", ":BufferLinePick<CR>", { noremap = true, silent = true, desc = "[S]elect" })
 
@@ -111,17 +111,21 @@ vim.api.nvim_set_keymap("n", "<Leader>bw", ":w<CR>", { noremap = true, silent = 
 -- Configure LSP settings --
 ----------------------------
 vim.lsp.handlers["textDocument/diagnostic"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	virtual_text = true,
-	signs = true,
-	update_in_insert = true,
+  virtual_text = true,
+  signs = true,
+  update_in_insert = true,
 })
 
 vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>cf",
-	":lua vim.lsp.buf.format({ async = true })<CR>",
-	{ noremap = true, silent = true, desc = "[F]ormat" }
+  "n",
+  "<Leader>cf",
+  ":lua vim.lsp.buf.format({ async = true })<CR>",
+  { noremap = true, silent = true, desc = "[F]ormat" }
 )
+vim.keymap.set("n", "<leader>cs", vim.lsp.buf.signature_help, {
+  buffer = bufnr,
+  desc = "[S]ignature",
+})
 
 ---------------------------
 -- Configure UI settings --
@@ -135,41 +139,41 @@ vim.api.nvim_set_keymap(
 -- Configure LSP --
 -------------------
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		local bufnr = args.buf
+  callback = function(args)
+    local bufnr = args.buf
 
-		vim.keymap.set("n", "<leader>ds", function()
-			vim.diagnostic.open_float(nil, {
-				focusable = false,
-				border = "rounded", -- Set border for the diagnostic float here
-				scope = "line", -- Show diagnostics for the current line
-			})
-		end, {
-			buffer = bufnr,
-			noremap = true,
-			silent = true,
-			desc = "[S]how",
-		})
+    vim.keymap.set("n", "<leader>ds", function()
+      vim.diagnostic.open_float(nil, {
+        focusable = false,
+        border = "rounded", -- Set border for the diagnostic float here
+        scope = "line", -- Show diagnostics for the current line
+      })
+    end, {
+      buffer = bufnr,
+      noremap = true,
+      silent = true,
+      desc = "[S]how",
+    })
 
-		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {
-			buffer = bufnr,
-			noremap = true,
-			silent = true,
-			desc = "[D]efinition",
-		})
+    vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {
+      buffer = bufnr,
+      noremap = true,
+      silent = true,
+      desc = "[D]efinition",
+    })
 
-		vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {
-			buffer = bufnr,
-			noremap = true,
-			silent = true,
-			desc = "[I]mplementation",
-		})
+    vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {
+      buffer = bufnr,
+      noremap = true,
+      silent = true,
+      desc = "[I]mplementation",
+    })
 
-		vim.keymap.set("n", "<leader>gr", vim.lsp.buf.definition, {
-			buffer = bufnr,
-			noremap = true,
-			silent = true,
-			desc = "[R]eferences",
-		})
-	end,
+    vim.keymap.set("n", "<leader>gr", vim.lsp.buf.definition, {
+      buffer = bufnr,
+      noremap = true,
+      silent = true,
+      desc = "[R]eferences",
+    })
+  end,
 })
