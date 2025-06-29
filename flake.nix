32cf786bf2929c00553dcec6d6483cb6c4004d38
ferrazzo <luca733@gmail.com>
2025-06-29 14:43:39 +0200

    fix: add intel driver

--- flake.nix
+++ flake.nix
@@ -8,49 +8,46 @@
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      ...
-    }@inputs:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      system = "x86_64-linux";
-      userList = [
-        {
-          username = "luca";
-          userTheme = "everforest"; # everforest or nordic
-        }
-      ];
-      host = "laptop";
-    in
-    {
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          system = system;
-          specialArgs = { inherit inputs; };
-          modules = [
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.users = pkgs.lib.listToAttrs (
-                map (user: {
-                  name = user.username;
-                  value = import ./home/home.nix {
-                    pkgs = pkgs;
-                    username = user.username;
-                    userTheme = user.userTheme;
-                  };
-                }) userList
-              );
-            }
-            (import ./configuration.nix { inherit pkgs host; })
-          ];
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+    system = "x86_64-linux";
+    userList = [
+      {
+        username = "luca";
+        userTheme = "everforest"; # everforest or nordic
+      }
+    ];
+    host = "laptop";
+  in {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.users = pkgs.lib.listToAttrs (
+              map (user: {
+                name = user.username;
+                value = import ./home/home.nix {
+                  pkgs = pkgs;
+                  username = user.username;
+                  userTheme = user.userTheme;
+                };
+              })
+              userList
+            );
+          }
+          (import ./configuration.nix {inherit pkgs host;})
+        ];
       };
     };
+  };
 }
