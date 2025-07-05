4bd2d411653378e9d8602af19eb1f569403cae20
ferrazzo <luca733@gmail.com>
2025-07-05 21:02:54 +0200

    chore: refactor

--- configuration.nix
+++ configuration.nix
@@ -121,9 +121,9 @@
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.luca = {
+  users.users.${username} = {
     isNormalUser = true;
-    description = "luca";
+    description = username;
     extraGroups = [
       "networkmanager"
       "wheel"
