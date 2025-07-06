179c3591df626914f60b025da4db04c8896bbb5f
ferrazzo <luca733@gmail.com>
2025-07-06 21:19:17 +0200

    feat(starship): add direnv and golang status

--- home/apps/starship/default.nix
+++ home/apps/starship/default.nix
@@ -8,7 +8,7 @@ in
       add_newline = true;
        direnv = {
         disabled = false;
-        format = "╱ [$loaded/$allowed ]($style)";
+        format = "╱ [$loaded / $allowed ]($style)";
         allowed_msg = "";
         not_allowed_msg = "";
         denied_msg = "";
