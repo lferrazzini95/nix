c5990c06df0a24ad37b4c17f6cd5a06e34b52a72
luca <luca733@gmail.com>
2025-07-11 15:01:44 +0200

    feat(config): disable hybernate because of wifi issues

--- configuration.nix
+++ configuration.nix
@@ -155,7 +155,7 @@
 
   # System & Nix Settings
   systemd.targets = {
-    # hybernate.enable = false;
+    hybernate.enable = false;
   };
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "24.11"; # Did you read the comment?
